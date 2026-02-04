1bfe6d2aeed1d62e958dcf026f234204616dec6c
kevin <kevin@jessid.com>
2026-02-04 20:49:08 +0000

    Format Demo1Applcation

--- src/main/java/com/example/demo_1/Demo1Application.java
+++ src/main/java/com/example/demo_1/Demo1Application.java
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 @RestController
 @EnableAutoConfiguration
-@SpringBootApplication
 public class Demo1Application {
 
 	@RequestMapping("/")
         String home() {
-                return "<h1 style='background-color: #002b36; color: yellow; font-family:'Titillium Web', sans-serif; padding: 10px; padding-left: 20px;'>Hello There!</h1>";
+                return "<h1 style='color: gray; font-family:'Titillium Web', sans-serif; padding: 10px; padding-left: 20px;}'>Hello There!</h1>";
+
         }
 
 	public static void main(String[] args) {
